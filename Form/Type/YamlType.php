8f99400b922f5e6b28dbfd3ced3b5198542e6917
ツ Liverbool <nukboon@gmail.com>
2015-06-16 19:30:59 +0700

    yam form type added.

---
+++ Form/Type/YamlType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace DoS\ResourceBundle\Form\Type;
+
+use DoS\ResourceBundle\Form\DataTransformer\YamlTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class YamlType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->addModelTransformer(new YamlTransformer($options['inline_level']))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        parent::setDefaultOptions($resolver);
+
+        $resolver->setDefaults(array(
+            'inline_level' => 10
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'textarea';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'dos_yaml';
+    }
+}
