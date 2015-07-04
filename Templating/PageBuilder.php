17d719c0397ca4853345d0e1bfd7a3d68d493a76
ツ Liverbool <nukboon@gmail.com>
2015-07-05 04:17:35 +0700

    add page builder to ui.

---
+++ Templating/PageBuilder.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace DoS\ResourceBundle\Templating;
+
+use Sylius\Bundle\SettingsBundle\Templating\Helper\SettingsHelper;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PageBuilder
+{
+    /**
+     * @var SettingsHelper
+     */
+    protected $settingsHelper;
+
+    /**
+     * @var array
+     */
+    protected $options = array();
+
+    //private $theme = null;
+
+    public function __construct(SettingsHelper $settingsHelper, $defaultOptions = array())
+    {
+        $this->settingsHelper = $settingsHelper;
+        $this->options = $defaultOptions;
+    }
+
+    /**
+     * Set page options.
+     *
+     * @param array $options
+     */
+    public function setOptions(array $options = array())
+    {
+        $options['inited'] = true;
+
+        // Resolve merged options
+        $resolver = new OptionsResolver();
+        $this->configureOptions($resolver);
+        $options = $resolver->resolve($options);
+        $this->options = array_merge($this->options, $options);
+
+        if (empty($this->options['blocks']['header'])) {
+            $this->options['header'] = false;
+        }
+
+        if (!$this->options['header']) {
+            $this->options['css'] = trim($this->options['css'].' no page header');
+        }
+    }
+
+    /**
+     * @param $name
+     * @param null $default
+     *
+     * @return null|string
+     */
+    public function getOption($name, $default = null)
+    {
+        return $this->getOptionValue($name, $this->options) ?: $default;
+    }
+
+    /**
+     * @param string     $option
+     * @param string     $printOut
+     * @param null|mixed $defaultOptionValue
+     *
+     * @return string
+     */
+    public function get($option, $printOut = '%s', $defaultOptionValue = null)
+    {
+        if ($value = $this->getOption($option, $defaultOptionValue)) {
+            if (is_array($value) || is_object($value)) {
+                return $value;
+            }
+
+            return sprintf($printOut, $value);
+        }
+
+        return;
+    }
+
+    /**
+     * @param string $steps
+     * @param array  $options
+     *
+     * @return null|string
+     */
+    private function getOptionValue($steps, array $options)
+    {
+        $value = $options;
+        $steps = explode('.', $steps);
+
+        foreach ($steps as $step) {
+            if (!array_key_exists($step, $value)) {
+                return;
+            }
+
+            $value = $value[$step];
+        }
+
+        return $value;
+    }
+
+    /**
+     * Configure the option resolver.
+     *
+     * @param OptionsResolver $resolver
+     */
+    protected function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired(array());
+
+        $resolver->setDefaults(array(
+            'id' => null,
+            'css' => null,
+            'charset' => 'UTF-8',
+            'title' => $this->settingsHelper->getSettingsParameter('general.title'),
+            'locale' => $this->settingsHelper->getSettingsParameter('general.locale'),
+            'metas' => array(
+                'keywords' => $this->settingsHelper->getSettingsParameter('general.meta_keywords'),
+                'description' => $this->settingsHelper->getSettingsParameter('general.meta_description'),
+            ),
+            'blocks' => null,
+            'styles' => null,
+            'scripts' => null,
+            'keywords' => null,
+            'decription' => null,
+            'breadcrumbs' => null,
+            'reset_script' => false,
+            'reset_style' => false,
+            'reset_meta' => false,
+            'reset_breadcrumb' => false,
+            'form' => null,
+            'header' => true,
+            'inited' => false,
+        ));
+
+        $resolver->setAllowedTypes(array(
+            'inited' => array('boolean'),
+            'header' => array('boolean'),
+            'metas' => array('null', 'array'),
+            'blocks' => array('null', 'array'),
+            'form' => array('null', 'array'),
+        ));
+    }
+}
---
+++ Twig/Extension/Page.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace DoS\ResourceBundle\Twig\Extension;
+
+use DoS\ResourceBundle\Templating\PageBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\RouterInterface;
+
+class Page extends \Twig_Extension
+{
+    /**
+     * @var PageBuilder
+     */
+    private $pageBuilder;
+
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * @var RequestStack
+     */
+    private $request;
+
+    public function __construct(PageBuilder $pageBuilder)
+    {
+        $this->pageBuilder = $pageBuilder;
+    }
+
+    /**
+     * @param ContainerInterface $container
+     */
+    public function setContainer(ContainerInterface $container)
+    {
+        $this->router = $container->get('router');
+        $this->request = $container->get('request_stack');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('ui_page', array($this, 'getUiFile')),
+            new \Twig_SimpleFunction('ui_page_is', array($this, 'isPageId')),
+            new \Twig_SimpleFunction('ui_page_get', array($this, 'getPageOption')),
+            new \Twig_SimpleFunction('ui_page_options', array($this, 'setPageOptions')),
+            new \Twig_SimpleFunction('ui_page_actions', array($this, 'getUiPageActions')),
+            new \Twig_SimpleFunction('ui_page_self', array($this, 'getUiPageSelf')),
+            new \Twig_SimpleFunction('ui_css_if', array($this, 'getCssIfMatchRoute')),
+        );
+    }
+
+    /**
+     * @param $file
+     *
+     * @return string
+     */
+    public function getUiFile($file)
+    {
+        return sprintf('ui:%s.html.twig', $file);
+    }
+
+    /**
+     * @param \Twig_Template $scope
+     * @param null|string    $name
+     *
+     * @return mixed|string
+     */
+    public function getUiPageActions(\Twig_Template $scope, $name = null)
+    {
+        $file = $scope->getTemplateName();
+        $pattern = '/ui\:(.*):(.*)\.html\.twig/';
+
+        if ($name) {
+            $file = preg_replace($pattern, sprintf('ui:$1/Actions:%s.html.twig', $name), $file);
+        } else {
+            $file = preg_replace($pattern, 'ui:$1/Actions:${2}.html.twig', $file);
+        }
+
+        return $file;
+    }
+
+    /**
+     * @param \Twig_Template $scope
+     * @param string         $name
+     *
+     * @return mixed
+     */
+    public function getUiPageSelf(\Twig_Template $scope, $name)
+    {
+        $file = $scope->getTemplateName();
+        $pattern = '/ui\:(.*):(.*)\.html\.twig/';
+
+        return preg_replace($pattern, sprintf('ui:$1:%s.html.twig', $name), $file);
+    }
+
+    /**
+     * @param array $options
+     */
+    public function setPageOptions(array $options)
+    {
+        $this->pageBuilder->setOptions($options);
+    }
+
+    /**
+     * @param string $option
+     * @param string $printOut
+     * @param null   $default
+     *
+     * @return string
+     */
+    public function getPageOption($option, $printOut = '%s', $default = null)
+    {
+        return $this->pageBuilder->get($option, $printOut, $default);
+    }
+
+    /**
+     * @param $id
+     *
+     * @return bool
+     */
+    public function isPageId($id)
+    {
+        return $this->pageBuilder->getOption('id') === $id;
+    }
+
+    /**
+     * @param string|bool $check
+     * @param string      $css
+     *
+     * @return null|string
+     */
+    public function getCssIfMatchRoute($check, $css = 'active')
+    {
+        if (is_string($check)) {
+            $matcher = $this->router->match($this->request->getCurrentRequest()->getPathInfo());
+            $check = $matcher['_route'] === $check;
+        }
+
+        return $check ? $css : null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getName()
+    {
+        return 'ui_page';
+    }
+}
