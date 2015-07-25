62157409067f42de854c84358bab0eefce443fe3
ツ Liverbool <nukboon@gmail.com>
2015-07-26 01:38:43 +0700

    add ajax_redirect listener to be default behavior resource.

---
+++ EventListener/AjaxRedirect.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DoS\ResourceBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+class AjaxRedirect
+{
+    /**
+     * Set 204 status and return location redirect for ajax redirect.
+     *
+     * @param FilterResponseEvent $event
+     */
+    public function setJsonRedirection(FilterResponseEvent $event)
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $request = $event->getRequest();
+
+        // no handle with ajax (except turbolinks)
+        if ($event->getResponse() instanceof RedirectResponse && $request->isXmlHttpRequest()) {
+            /** @var FlashBagInterface $flashes */
+            $flashes = $event->getRequest()->getSession()->getFlashBag();
+            $headers = array(
+                'x-location' => $event->getResponse()->getTargetUrl(),
+                'x-flashes' => $flashes ? json_encode($flashes->all()) : null
+            );
+
+            $event->setResponse(new Response(null, 204, $headers));
+        }
+    }
+}
