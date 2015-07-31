c5b030b1aa57b9e1a44669e9c6cd688d53e5f675
ツ Liverbool <nukboon@gmail.com>
2015-07-31 16:14:45 +0700

    improve ajax redirect event.

--- EventListener/AjaxRedirect.php
+++ EventListener/AjaxRedirect.php
@@ -24,10 +24,20 @@ class AjaxRedirect
 
         // no handle with ajax (except turbolinks)
         if ($event->getResponse() instanceof RedirectResponse && $request->isXmlHttpRequest()) {
+            $uri = rtrim(explode('?', $request->getUri())[0], '/');
+            $target = rtrim(explode('?', $targetUrl = $event->getResponse()->getTargetUrl())[0], '/');
+
+            // when just sf route `/` matching redirect
+            // most of index route end with `/` but we just enter `resources`
+            // eg. `/users` may redirect to `/users/`
+            if ($uri === $target) {
+                return;
+            }
+
             /** @var FlashBagInterface $flashes */
             $flashes = $event->getRequest()->getSession()->getFlashBag();
             $headers = array(
-                'x-location' => $event->getResponse()->getTargetUrl(),
+                'x-location' => $targetUrl,
                 'x-flashes' => $flashes ? json_encode($flashes->all()) : null
             );
 
