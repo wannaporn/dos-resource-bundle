4e1eedd916998210dd65c1efae3cf6082e304f10
ツ Liverbool <nukboon@gmail.com>
2015-08-28 23:41:16 +0700

    add `AbstractProvider`

---
+++ Provider/AbstractProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DoS\ResourceBundle;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use DoS\ResourceBundle\Doctrine\ORM\EntityRepository;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+
+/**
+ * @author liverbool <nukboon@gmail.com>
+ */
+abstract class AbstractProvider
+{
+    /**
+     * @var ObjectManager
+     */
+    protected $manager;
+
+    /**
+     * @var string
+     */
+    protected $dataClass;
+
+    /**
+     * @var RepositoryInterface
+     */
+    protected $repository;
+
+    public function __construct(EntityRepository $repository)
+    {
+        $this->repository = $repository;
+        $this->manager = $repository->getManager();
+        $this->dataClass = $repository->getEntityName();
+    }
+}
