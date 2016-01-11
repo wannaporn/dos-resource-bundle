631856242905c74293b6c152cfded6e9ddc2bf35
ツ Liverbool <nukboon@gmail.com>
2016-01-11 20:55:53 +0700

    add originator feature.

--- Model/OriginContextInterface.php
+++ Model/OriginContextInterface.php
@@ -11,4 +11,9 @@ interface OriginContextInterface extends UuidAwareInterface
      * @return string
      */
     public static function getOriginalAlias();
+
+    /**
+     * @return string get_class($this);
+     */
+    public static function getOriginalType();
 }
--- Originator/Originator.php
+++ Originator/Originator.php
@@ -2,9 +2,39 @@
 
 namespace DoS\ResourceBundle\Originator;
 
+use DoS\ResourceBundle\Model\OriginContextInterface;
+use Sylius\Component\Originator\Model\OriginAwareInterface;
 use Sylius\Component\Originator\Originator\Originator as BaseOriginator;
+use Sylius\Component\Resource\Exception\UnexpectedTypeException;
 
 class Originator extends BaseOriginator
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function setOrigin(OriginAwareInterface $originAware, $origin)
+    {
+        if (!is_object($origin)) {
+            throw new UnexpectedTypeException($origin, 'object');
+        }
 
+        /** @var \DoS\ResourceBundle\Model\OriginAwareInterface $originAware */
+        if ($origin instanceof OriginContextInterface) {
+            $originAware->setOriginAlias($origin->getOriginalAlias());
+            $originAware->setOriginType($origin->getOriginalType());
+            $originAware->setOriginId($origin->getId());
+
+            return;
+        }
+
+        if (null === $id = $this->accessor->getValue($origin, $this->identifier)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Origin %s is not set.',
+                $this->identifier
+            ));
+        }
+
+        $originAware->setOriginId($id);
+        $originAware->setOriginType(get_class($origin));
+    }
 }
