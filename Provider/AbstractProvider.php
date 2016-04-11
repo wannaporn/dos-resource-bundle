28dfad241447cae4b3040c8e28ccc75cde61dee2
ツ Liverbool <nukboon@gmail.com>
2016-04-11 16:20:32 +0700

    add feature to resource provider.

--- Provider/AbstractProvider.php
+++ Provider/AbstractProvider.php
@@ -6,6 +6,7 @@ use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ORM\EntityManager;
 use DoS\ResourceBundle\Doctrine\RepositoryInterface;
 use DoS\ResourceBundle\Factory\FactoryInterface;
+use Sylius\Component\Resource\Model\ResourceInterface;
 
 /**
  * @author liverbool <nukboon@gmail.com>
@@ -38,5 +39,80 @@ abstract class AbstractProvider
         $this->factory = $factory;
         $this->manager = $repository->getManager();
         $this->dataClass = $repository->getEntityName();
+    }
+
+    /**
+     * @return ResourceInterface
+     */
+    public function createNew()
+    {
+        return $this->factory->createNew();
+    }
+
+    /**
+     * @param ResourceInterface $resource
+     */
+    public function persist(ResourceInterface $resource)
+    {
+        $this->manager->persist($resource);
+    }
+
+    /**
+     * @param ResourceInterface|null $resource
+     */
+    public function flush(ResourceInterface $resource = null)
+    {
+        $this->manager->flush($resource);
+    }
+
+    /**
+     * @param ResourceInterface $resource
+     * @param bool $single
+     */
+    public function save(ResourceInterface $resource, $single = false)
+    {
+        $this->manager->persist($resource);
+        $this->manager->flush($single ? $resource : null);
+    }
+
+    /**
+     * @param $id
+     *
+     * @return ResourceInterface
+     */
+    public function find($id)
+    {
+        return $this->repository->find($id);
+    }
+
+    /**
+     * @return ResourceInterface[]
+     */
+    public function findAll()
+    {
+        return $this->repository->findAll();
+    }
+
+    /**
+     * @param array $criteria
+     * @param array|null $orderBy
+     * @param null $limit
+     * @param null $offset
+     *
+     * @return ResourceInterface[]
+     */
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    {
+        return $this->repository->findBy($criteria, $orderBy, $limit, $offset);
+    }
+
+    /**
+     * @param array $criteria
+     *
+     * @return ResourceInterface[]
+     */
+    public function findOneBy(array $criteria)
+    {
+        return $this->repository->findOneBy($criteria);
     }
 }
